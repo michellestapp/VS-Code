7a8fbcd66d6ebc0848c8afdc44b72d65f36fb14c
michellestapp <mstapp@rocketmail.com>
2023-04-06 12:02:41 -0500

    corrected Task 1 per instructions

--- algorithms.py
+++ algorithms.py
@@ -4,12 +4,28 @@
 # Leave a comment above the function stating the time complexity.
 # Send a screenshot of your solution and time complexity comment to your personal instructors chat.
 
-user_number = int(input(" Enter a number to find if it's even or odd: "))
+# TIME COMPLEXITY =>  O(1)
 
+def is_even(user_number):
+    
+    e_or_o = user_number%2
 
-e_or_o = user_number%2
+    if e_or_o == 0:
+        return True
+    else:
+        return False
 
-if e_or_o == 0:
-    print(f' The number {user_number} is EVEN!')
+user_number = int(input(" \n Enter a number to find if it's even: \n"))
+if is_even(user_number):
+    print(f' The number {user_number} is EVEN!\n')
 else:
-    print(f' The number {user_number} is ODD')
+    print(f' The number {user_number} is ODD\n')
+
+
+# Task 2: Less than 100
+# Given a list of numbers, determine if each item in the list is lower than 100.
+# The function should take in the list as a parameter and return a boolean value (False if there are any numbers greater than or equal to 100, True if all numbers are less than 100).
+# Leave a comment above the function stating the time complexity.
+# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+
+# 
