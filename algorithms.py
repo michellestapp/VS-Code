7c744bad9bdae0fd7fa72ccb3b6f5fe380476ad6
michellestapp <mstapp@rocketmail.com>
2023-04-06 15:59:19 -0500

    Added tasks 3 and 4

--- algorithms.py
+++ algorithms.py
@@ -28,20 +28,97 @@
 # Leave a comment above the function stating the time complexity.
 # Send a screenshot of your solution and time complexity comment to your personal instructors chat.
 
-# TIME COMPLEXITY =>  O(1) 
+# TIME COMPLEXITY =>  O(n) 
 
-def less_than_100(list):
-    all_lt100 = True
-    for num in list :
-        if num >= 100:
-           all_lt100 = False
-           break
-    return all_lt100
-my_list = [10,54, 57, 75, 2]
+# def less_than_100(list):
+#     all_lt100 = True
+#     for num in list :
+#         if num >= 100:
+#            all_lt100 = False
+#            break
+#     return all_lt100
+# my_list = [10,54, 57, 75, 2]
 
-if less_than_100(my_list) == True:
-    print('\n All numbers under 100\n')
+# if less_than_100(my_list) == True:
+#     print('\n All numbers under 100\n')
+# else:
+#     print('\n You have at least one number that is greater than or equal to 100\n')
+
+
+# Task 3: Repeated Names
+# Given a list of names, determine if any names are contained in the list more than once.
+# The function should take in the list as a parameter and return a boolean value (True if there are any repeated names, False if there are no repeats).
+# Leave a comment above the function stating the time complexity.
+# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+    # counter_a = 0
+    # counter_b = 1
+    # num_names = len(list)
+    # for counter_a in range(num_names -1):
+    #     for counter_b in range(counter_a+1,num_names -1):
+    #         if list[counter_a] == list[counter_b]:
+    #             return True
+    #             break
+    #         else:
+    #             counter_b += 1
+    #     counter_a += 1
+    #     counter_b = counter_a +1
+    # return False
+
+
+# # TIME COMPLEXITY =>  O(n) Two loops that cycle through the list
+
+def repeated_names(list):
+
+    list_2 = []
+    for name_one in list:
+        if name_one in list_2:
+            return True
+            break
+        else:
+            list_2.append(name_one)
+    return False
+
+list = ['Brian','Brian','Michelle','David']
+
+if repeated_names(list) == True:
+    print('\n There are duplicated names in the list\n')
 else:
-    print('\n You have at least one number that is greater than or equal to 100\n')
+    print('\n There are NO duplicated names in the list\n')
+
+
+#    Task 4: Sort List
+# Given a list of numbers, manually sort the list into ascending order (may not use built in .sort() method).
+# Suggested strategy:
+# Starting with the first two numbers, compare them to see which is larger. 
+# Place the lower number to the left and the higher number to the right.
+# Iterate through the list, checking each pair of numbers one at a time.
+# Repeat from the start until the entire list is sorted.
+# Leave a comment above the function stating the time complexity.
+# Send a screenshot of your solution and time complexity comment to your personal instructors chat.
+
+# # TIME COMPLEXITY =>  n^2 : a loop within a loop
+     
+def sort_list(list):
+    i = 0
+    j = 1
+
+    num_indexes = len(list) - 1
+
+    for i in range(0,num_indexes):
+        for j in range(i, num_indexes+1):
+            if list[i] > list[j]:
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+                print()
+                print(list)
+
+
+my_list = [24,5,61,0.5,19,0,1002, 2]
+
+sort_list(my_list)
+
+
+
 
 
